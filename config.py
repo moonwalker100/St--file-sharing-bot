6f02f287d802e168eaf8246a510db8947b41e19e
{remo} <moonwalker1092@gmail.com>
2026-03-17 23:11:38 +0530

    Update bot configuration with new token and images

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather, --⚠️ REQUIRED--
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8239128629:AAH2mllIgL9d9Fgb5MgtJOxrapo5svUikaA")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8239128629:AAH2mll")
 #Your API ID from my.telegram.org --⚠️ REQUIRED--
 APP_ID = int(os.environ.get("APP_ID", "24371796"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "8121c78f4b8b31e88cc2623d1277338d")
+API_HASH = os.environ.get("API_HASH", "8121c78f4b8b3)
 
 #Your db channel Id --⚠️ REQUIRED--
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003511623672"))
@@ -34,21 +34,22 @@ VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://dattebayo56:dattebayo56@a
 DBV_NAME = os.environ.get("VERIFY_DBNAME", "OrewaSanjiiBots")
 
 
-TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://envs.sh/H29.jpg")
+TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://graph.org/file/86e95130c66457861907e-dda4b5a0929a967af8.jpg")
 
 
 #Tutorial video for the user of your shortner on how to download.
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
 
 
-START_PIC = os.environ.get("START_PIC", "https://envs.sh/H29.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/H29.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/538b82fce880148934408-b78d6217e4a93ce60c.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/cb972656c98ddedaacd81-ca93587b2ace0a5033.jpg")
 
                         
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/H29.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "https://graph.org/file/c22ccad28832aed94dbf1-49c96f1f7912938b86.jpg https://graph.org/file/19e15ec483ec1b7656b09-fef4ad2d01c0bc4552.jpg https://graph.org/file/babf5708d7b0ea534a17c-0eb69cf7275020b3fe.jpg https://graph.org/file/c31acfb82bcb6b84cf0db-be7324cab11728ef6d.jpg https://graph.org/file/751ee02f7a14fe4ce2654-bdd2466f2072f43072.jpg https://graph.org/file/8f6cc424fd39cc1795a9a-b391b8231829437915.jpg https://graph.org/file/6b3b30c050fcf3da727b4-60401beca28ed88771.jpg https://graph.org/file/e359b78b725dc87ef9743-24d5f30754037d7926.jpg https://graph.org/file/514e7cfb6c3feb200f7bc-ed0a96dd3d74c134c3.jpg https://graph.org/file/7ab8a716cb00a542acdac-677062efb59b3f5c44.jpg https://graph.org/file/a617481d19aa04ace0c7c-ab1b60b42572ba17ae.jpg https://graph.org/file/a0aecb9fe2fe82e47ee29-407fa0435de3264296.jpg https://graph.org/file/580287fb4b172bd0a7919-8085660baa0f8326c9.jpg https://graph.org/file/c57ea68eab59cee31758b-e4ccde7a74cdf7c049.jpg")).split() #Required
+
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
